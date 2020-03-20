0ca32b379f01d99fe5687169a859a7f51ad6cb9f
developerfitz <fitz3364@gmail.com>
2020-03-20 12:41:04 -0400

    fix(DB): query changed to align with spec

--- step1-clean/query.py
+++ step1-clean/query.py
@@ -1,16 +1,18 @@
-from db import session, SQLImage
-
-def filterThumbnails():
-  query = session.query(SQLImage).filter_by(isThumbnail="True").all()
-
-  for row in query:
-    print(row)
+from db import session
+from models import Images, Thumbnails
 
 
-def queryAll():
-  query = session.query(SQLImage).all()
-  for row in query:
-    print(row)
+def filterByName():
+  inputName = input('Input exact filename: ')
+  imageQuery = session.query(Images).filter_by(filename = inputName).all()
+  thumbnailQuery = session.query(Thumbnails).filter_by(original = inputName).all()
+
+  if not imageQuery and not thumbnailQuery:
+    print('Not found in DB.')
+    print('Ensure filename is correct (including extension).')
+  else:
+    print(imageQuery)
+    print(thumbnailQuery)
 
 
-filterThumbnails()
+filterByName()
